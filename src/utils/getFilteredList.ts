9288848ac3e3c25033ca2d11ded7570cdd5215bc
simeunseo <rkdcjfehowl@naver.com>
2023-12-20 02:54:36 +0900

    feat: 맞팔리스트와 맞팔아닌 리스트 필터링하는 util 함수 구현

---
+++ src/utils/getFilteredList.ts
@@ -0,0 +1,18 @@
+import { User, getFollowersList, getFollowingList } from 'apis/getFollowData';
+
+const getFilteredLists = async (token?: string) => {
+  const followingList: User[] = await getFollowingList(token);
+  const followersList: User[] = await getFollowersList(token);
+
+  const f4fList = followingList.filter((followingUser) => {
+    return followersList.some((follower) => follower.id === followingUser.id);
+  });
+
+  const notF4fList = followersList.filter((follower) => {
+    return !followingList.some((followingUser) => followingUser.id === follower.id);
+  });
+
+  return { f4fList, notF4fList };
+};
+
+export default getFilteredLists;
