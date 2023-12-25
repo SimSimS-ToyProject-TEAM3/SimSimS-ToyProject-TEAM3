7bb8d60c7af1620728036dacdd33f92c10d255f9
simeunseo <rkdcjfehowl@naver.com>
2023-12-25 17:05:19 +0900

    feat: 전체선택/해제 구현

--- src/components/follow-list/CheckBox.tsx
+++ src/components/follow-list/CheckBox.tsx
@@ -1,12 +1,19 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 interface CheckboxProps {
+  isAllChecked: boolean;
   username: string;
   checkList: string[];
   setCheckList: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-export default function Checkbox({ username, checkList, setCheckList }: CheckboxProps) {
+export default function Checkbox({ isAllChecked, username, checkList, setCheckList }: CheckboxProps) {
+  let isChecked = false;
+
+  useEffect(() => {
+    isChecked = checkList.includes(username);
+  }, [isAllChecked]);
+
   const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget.value;
 
@@ -19,7 +26,6 @@ export default function Checkbox({ username, checkList, setCheckList }: Checkbox
     });
   };
 
-  const isChecked = checkList.includes(username);
   return (
     <>
       <label htmlFor={username}>
@@ -27,7 +33,7 @@ export default function Checkbox({ username, checkList, setCheckList }: Checkbox
           type="checkbox"
           id={username}
           name={username}
-          checked={isChecked}
+          checked={isAllChecked ? true : isChecked}
           value={username}
           onChange={handleOnChange}
         />
